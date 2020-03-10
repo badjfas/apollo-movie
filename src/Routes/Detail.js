853ebb71aeff0122609747eb8297056ffd4a7aa9
배진우 <bjwkor@naver.com>
2020-03-10 21:41:11 +0900

    div

--- src/Routes/Detail.js
+++ src/Routes/Detail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useParams} from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 import styled from "styled-components";
@@ -9,14 +9,14 @@ const Container = styled.div`
   background-image: linear-gradient(-45deg, #d754ab, #fd723a);
   width: 100%;
   display: flex;
-  justify-content: space-around;
+  justify-content: flex;
   align-items: center;
   color: white;
 `;
 
 const Column = styled.div`
-  margin-left: 10px;
-  width: 50%;
+  margin-left: 20px;
+  width: 100%;
 `;
 
 const Title = styled.h1`
@@ -42,14 +42,29 @@ const Poster = styled.div`
   background-position: center center;
 `;
 
-const Footer = styled.footer`
-    height:100%;
-    background-image: url(${props => props.bg});
-  background-size: cover;
+const Movies = styled.div`
+  height: 400px;
+  border-radius: 7px;
+  width: 100%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  background-color: transparent;
+`;
 
-    width:100%;
-    display:flex;
-    `;
+const MovieContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 100%;
+`;
+
+const MoviePoster = styled.div`
+  background-image: url(${props => props.bg});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -61,40 +76,46 @@ const GET_MOVIE = gql`
       medium_cover_image
       description_intro
     }
-    suggestions(id:$id){
-        id
-        title
-        medium_cover_image
+    suggestions(id: $id) {
+      id
+      title
+      medium_cover_image
     }
   }
 `;
 
-export default ({bg}) => {
-
+export default ({ bg }) => {
   const { id } = useParams();
   const { loading, data } = useQuery(GET_MOVIE, {
-    variables: { id:parseInt(id) }
+    variables: { id: parseInt(id) }
   });
-  
+
   return (
     <Container>
       <Column>
         <Title>{loading ? "Loading...." : data.movie.title}</Title>
+        {!loading && (
+          <Poster bg={!loading && data.movie.medium_cover_image}></Poster>
+        )}
         {!loading && data.movie && (
           <>
             <Subtitle>
               {data?.movie?.language} · 👑{data?.movie?.rating}
             </Subtitle>
-            <Description>
-                {data&&data.movie.description_intro}
-            </Description>
+            <Description>{data && data.movie.description_intro}</Description>
           </>
         )}
       </Column>
       {!loading && (
         <Poster bg={!loading && data.movie.medium_cover_image}></Poster>
       )}
-      {!loading&&data&&data.suggestions.map(sm=><Footer  key={sm.id} bg={sm.medium_cover_image}></Footer>)}
+      {!loading&&data?.suggestions.map(sm => (
+        <Movies>
+        <Link to={`/${sm.id}`}>
+        <Poster key={sm.id} id={sm.id} bg={sm.medium_cover_image} />
+        </Link>
+        </Movies>
+      ))}
     </Container>
   );
 };
